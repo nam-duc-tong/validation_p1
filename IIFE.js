3c86e54e5b917946a06b62e97f5eecc2000181a0
Tống Đức Nam <637849@sv.vnua.edu.vn>
2022-01-06 23:20:19 +0700

    update

--- IIFE.js
+++ IIFE.js
@@ -1,18 +1,18 @@
-const app = (function(){
-    //private 
-    const cars = [];
-    return{
-        get(index){
-            return cars[index];
-        },
-        add(car){
-            cars.push(car);
-        },
-        edit(index,car){
-            cars[index] = car;
-        },
-        delete(index){
-            cars.splice(index,1);
-        }
-    }
-})()
+// const app = (function(){
+//     //private 
+//     const cars = [];
+//     return{
+//         get(index){
+//             return cars[index];
+//         },
+//         add(car){
+//             cars.push(car);
+//         },
+//         edit(index,car){
+//             cars[index] = car;
+//         },
+//         delete(index){
+//             cars.splice(index,1);
+//         }
+//     }
+// })()
